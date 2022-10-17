055ecefbcd2d83eec43c66b0193574b6f7fc2ea6
bose-abhishek <85727318+bose-abhishek@users.noreply.github.com>
2022-10-17 13:20:20 +0530

    Add files via upload

--- delete_simple_fio.sh
+++ delete_simple_fio.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 server=`grep server config.file | awk -F "=" '{print $2}'`
-oc delete -f fio.yaml
+device_type=`grep storage_type config.file | awk -F "=" '{print $2}'`
+
+oc delete -f client.yaml
+
 for ((i=0;i<$server;i++));
 do
 export srv=${i}
-envsubst < server.yaml | oc delete -f -
-sleep 10
+if [ ${device_type} == "ocs-storagecluster-ceph-rbd" ]; then
+	envsubst < blk-server.yaml | oc delete -f -
+	sleep 10
+elif [ ${device_type} == "ocs-storagecluster-cephfs" ]; then
+	envsubst < fs-server.yaml | oc delete -f -
+	sleep 10
+fi
+
 envsubst < blk-pvc.yaml | oc delete -f -
 done
-
--- simple_fio.sh
+++ simple_fio.sh
@@ -17,18 +17,38 @@ fi
 # Creating the fio job file from config
 #---------------------------------------------------------
 file=job.fio
+pfile=prefill.fio
+
 echo "[global]" > ${file}
+
 echo "ioengine=libaio" >> ${file}
-tail -n +20 config.file >> ${file}
-echo "" >> ${file}
-echo "[device]" >> ${file}
+
+grep "^direct" config.file >> ${file}
+
+grep "^numjobs" config.file >> ${file}
+
+grep "^size" config.file >> ${file}
+
 if [ `grep storage_type config.file | awk -F "=" '{print $2}'` == "ocs-storagecluster-ceph-rbd" ]; then
-	echo "filename=/dev/rbd0" >> ${file}
+	echo "filename=/dev/rbd" >> ${file}
 elif [ `grep storage_type config.file | awk -F "=" '{print $2}'` == "ocs-storagecluster-cephfs" ]; then
 	echo "directory=/mnt/pvc" >> ${file}
 else
 	echo "Job file not configured properly as storage_type in config file is unknown"
 fi
+
+echo "" >> ${file}
+cp ${file} ${pfile}
+
+echo -e "[fio_test] \ntime_based=0 \nrw=write \nbs=128K \niodepth=1 \ngroup_reporting" >> ${pfile}
+
+echo -e "[fio_test]" >> ${file}
+grep "^rw" config.file >> ${file}
+grep "^bs" config.file >> ${file}
+grep "^time_based" config.file >> ${file}
+grep "^runtime" config.file >> ${file}
+grep "^iodepth" config.file >> ${file}
+echo "group_reporting" >> ${file}
 
 #==========================================================
 # Create fio server and client pods
@@ -36,33 +56,55 @@ fi
 server=`grep server config.file | awk -F "=" '{print $2}'`
 sample=`grep sample config.file | awk -F "=" '{print $2}'`
 storage=`grep "storage(Gi)" config.file | awk -F "=" '{print $2}'`
-final_serverIPs=""
+fio_args_prefill=""
+fio_args_job=""
 
 export sample=${sample}
 export storage=${storage}
-#> final_serverIPs
+
 for ((i=0;i<$server;i++));
 do
 	export srv=${i}
-	envsubst < blk-pvc.yaml | oc create -f -
-	sleep 10
-	envsubst < server.yaml | oc create -f -
-	sleep 10
+	if [ `grep storage_type config.file | awk -F "=" '{print $2}'` == "ocs-storagecluster-ceph-rbd" ]; then
+		envsubst < blk-pvc.yaml | oc create -f -
+		sleep 20
+		envsubst < blk-server.yaml | oc create -f -
+	elif [ `grep storage_type config.file | awk -F "=" '{print $2}'` == "ocs-storagecluster-cephfs" ]; then
+		envsubst < fs-pvc.yaml | oc create -f -
+		sleep 20
+		envsubst < fs-server.yaml | oc create -f -
+	fi
+	sleep 20
 	pod=`echo fio-server${i}`
 	oc cp job.fio ${namespace}/${pod}:/tmp/
+	oc cp prefill.fio ${namespace}/${pod}:/tmp/
 done
+
 for ((i=0;i<$server;i++));
 do
 	pod=`echo fio-server${i}`
-	serverIP=`oc get pod ${pod} --template '{{.status.podIP}}'`
-	final_serverIPs=`echo ${final_serverIPs} --client=${serverIP} --remote-config /tmp/job.fio `
+	serverIP=`oc get pod ${pod} -n ${namespace} --template '{{.status.podIP}}'`
+	fio_args_prefill=`echo ${fio_args_prefill} --client=${serverIP} --remote-config /tmp/prefill.fio `
+	#fio_args_prefill=`echo ${fio_args_prefill} --client=ip6:${serverIP} --remote-config /tmp/prefill.fio `
+	fio_args_job=`echo ${fio_args_job} --client=${serverIP} --remote-config /tmp/job.fio `
+	#fio_args_job=`echo ${fio_args_job} --client=ip6:${serverIP} --remote-config /tmp/job.fio `
+	sleep 20
 done
 
-echo ${final_serverIPs} > final_serverIPs
-export final_serverIPs=$(cat final_serverIPs)
+#echo ${fio_args_prefill}
+#echo ${fio_args_job}
+
+echo ${fio_args_prefill} > fio_args_prefill
+echo ${fio_args_job} > fio_args_job
+
+export fio_args_prefill=$(cat fio_args_prefill)
+export fio_args_job=$(cat fio_args_job)
+
 sleep 10
-envsubst < fio.yaml | oc create -f -
-rm -f final_serverIPs
+envsubst < client.yaml | oc create -f -
+
+rm -f fio_args_prefill
+#rm -f fio_args_job
 
 #---------------
 # End of script
