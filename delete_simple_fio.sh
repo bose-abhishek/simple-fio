d01baae393b2b4e4e4272cbaa4d05c4c06e7aa96
Abhishek Bose <85727318+bose-abhishek@users.noreply.github.com>
2024-08-10 03:05:15 +0530

    Update delete_simple_fio.sh for hcp

--- delete_simple_fio.sh
+++ delete_simple_fio.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
 
-oc get cm | grep fio
-if [ `echo $?` -eq 0 ]; then
-        for cm in $(oc get cm -oname | grep fio); do oc delete $cm; done
-fi
+namespace=simple-fio
+platform=`grep "^platform" config.file | awk -F "=" '{print $2}'`
 
-oc get pods | grep fio-server
-if [ `echo $?` -eq 0 ]; then
-        for pod in $(oc get pods -oname | grep fio-server); do oc delete $pod; done
-fi
+delete_setup () {
 
-if [[ $(oc get pods | grep fio-client | awk '{print $1}') == "fio-client" ]];
-        then
-        oc delete pod fio-client
-fi
+        oc project ${namespace}
+        
+        oc get cm | grep fio
+        if [ `echo $?` -eq 0 ]; then
+                for cm in $(oc get cm -oname | grep fio); do oc delete $cm; done
+        fi
+        
+        oc get pods | grep fio-server
+        if [ `echo $?` -eq 0 ]; then
+                for pod in $(oc get pods -oname | grep fio-server); do oc delete $pod; done
+        fi
+        
+        if [[ $(oc get pods | grep fio-client | awk '{print $1}') == "fio-client" ]];
+                then
+                oc delete pod fio-client
+        fi
+        
+        if [[ $(oc get pods | grep fio-prefill | awk '{print $1}') == "fio-prefill" ]];
+                then
+                oc delete pod fio-prefill
+        fi
+        
+        oc get pvc | grep fio-pv-claim
+        if [ `echo $?` -eq 0 ]; then
+                for pvc in $(oc get pvc -oname | grep fio-pv-claim); do oc delete $pvc; done
+        fi
+}
 
-if [[ $(oc get pods | grep fio-prefill | awk '{print $1}') == "fio-prefill" ]];
-        then
-        oc delete pod fio-prefill
-fi
+if [ $platform == "bm" ]; then
+        delete_setup
+elif [ $platform == "hcp" ]; then
+        for consumer in $(cat consumers.lst)
+        do
+                oc config use $consumer
+                delete_setup
+        done
 
-oc get pvc | grep fio-pv-claim
-if [ `echo $?` -eq 0 ]; then
-        for pvc in $(oc get pvc -oname | grep fio-pv-claim); do oc delete $pvc; done
 fi
