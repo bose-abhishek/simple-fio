1266fc792ef70b8e1906ff0298f88816bb35ef33
bose-abhishek <abose@redhat.com>
2025-02-03 13:57:30 +0530

    updated for hcp

--- delete_simple_fio.sh
+++ delete_simple_fio.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-server=`grep server config.file | awk -F "=" '{print $2}'`
-storage_type=`grep "^storage_type" config.file | awk -F "=" '{print $2}'`
-
-oc delete -f client.yaml
-
-for ((i=0;i<$server;i++));
-do
-export srv=${i}
-if [[ ${storage_type} =~ "ceph-rbd" ]]; then
-	envsubst < blk-server.yaml | oc delete -f -
-	sleep 10
-elif [[ ${storage_type} =~ "cephfs" ]]; then
-	envsubst < fs-server.yaml | oc delete -f -
-	sleep 10
-fi
-
-envsubst < blk-pvc.yaml | oc delete -f -
-done
-=======
 namespace=simple-fio
 platform=`grep "^platform" config.file | awk -F "=" '{print $2}'`
 
@@ -63,4 +43,3 @@ elif [ $platform == "hcp" ]; then
         done
 
 fi
->>>>>>> source/main
